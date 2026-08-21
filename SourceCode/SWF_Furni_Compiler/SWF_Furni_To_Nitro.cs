27ccf04112282fa7882a27a41baa66844068b7cb
duckietm <troop@mailtjes.com>
2026-08-21 16:26:28 +0200

    🆙 ramp up the size

--- SourceCode/SWF_Furni_Compiler/SWF_Furni_To_Nitro.cs
+++ SourceCode/SWF_Furni_Compiler/SWF_Furni_To_Nitro.cs
@@ -144,8 +144,8 @@ namespace Habbo_Downloader.Compiler
                     canonicalMapping,        // canonical mapping argument
                     disableCleanKey: false,  // set as desired
                     numRows: 10,
-                    maxWidth: 11266,
-                    maxHeight: 12800
+                    maxWidth: 12550,
+                    maxHeight: 12850
                 );
 
                 if (spriteSheetPath == null || spriteSheetData == null)
